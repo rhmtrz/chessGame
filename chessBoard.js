46d28d283cc79d3d14fa01916060285c172a1905
Rahmatullah Rezaei <rhmt@Rahmatullahs-Mac-mini.local>
2017-11-13 16:45:19 +0900

    chess table by tr

--- chessBoard.js
+++ chessBoard.js
@@ -1,9 +1,9 @@
+
+/*
 const chessBoardModel =
   [[null, null, null],
   [null, "queen", null],
   [null, null, "king"]];
-
-
 
 function showBoard(chessBoardModel){
   var chessBoard = '';
@@ -15,6 +15,7 @@ function showBoard(chessBoardModel){
       if(theCell == null){
         if ((y + x) % 2 === 0) {
           chessBoard += '#';
+          chessBoard.element.style.wid
         } else {
           chessBoard += '*';
         }
@@ -28,9 +29,55 @@ function showBoard(chessBoardModel){
   }
   console.log(chessBoard);
 }
-
-
 showBoard(chessBoardModel);
+*/
+var id = 0;
+var chessModel = [
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null]];
+
+function creatTable() {
+  var table = document.getElementById('table');
+  for (var r = 0; r < chessModel.length; r++) {
+    var row = document.createElement("tr");
+    console.log(row)
+    for (var c = 0; c < chessModel.length; c++) {
+      var cell = document.createElement("td");
+      console.log(cell);
+      cell.width = "70px";
+      cell.height = "70px";
+      cell.id = id++;
+
+      if ((r + c) % 2 == 0) {
+        cell.style.backgroundColor = "black";
+      } if ((r + c) % 2 == 1) {
+        cell.style.backgroundColor = "gainsboro";
+      }
+      row.appendChild(cell);
+
+    }
+    table.appendChild(row);
+  }
+}
+
+creatTable();
+
+
+
+
+
+
+
+
+
+
+
 
 /*
   1. distinguish pieces on vie like king=>k or queen=>q
@@ -43,7 +90,5 @@ showBoard(chessBoardModel);
       hint: readline()
 
   3. distinguish user and enemy on model and view
-
-
 
 */
