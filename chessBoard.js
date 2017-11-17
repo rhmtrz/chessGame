5c247d6bec55b553593b5f8ddeace183588fc640
Rahmatullah Rezaei <rhmt@Rahmatullahs-Mac-mini.local>
2017-11-17 22:12:49 +0900

    playGame

--- chessBoard.js
+++ chessBoard.js
@@ -32,10 +32,10 @@ function showBoard(chessBoardModel){
 showBoard(chessBoardModel);
 */
 var id = 0;
-var chessModel = [
+var chessBoard = [
+  [null, null, null, "King", null, null, null, null],
   [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
-  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, "queen", null, null, null],
   [null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null, null],
@@ -44,36 +44,120 @@ var chessModel = [
 
 function creatTable() {
   var table = document.getElementById('table');
-  for (var r = 0; r < chessModel.length; r++) {
+  for (var r = 0; r < chessBoard.length; r++) {
     var row = document.createElement("tr");
     console.log(row)
-    for (var c = 0; c < chessModel.length; c++) {
+    for (var c = 0; c < chessBoard.length; c++) {
       var cell = document.createElement("td");
-      console.log(cell);
+    //  var cellText = document.createTextNode("Cell");
+    //  console.log(cellText);
+    //  console.log();
       cell.width = "70px";
       cell.height = "70px";
-      cell.id = id++;
+      cell.id = r + "_" + c;
+      cell.textContent = chessBoard[r][c];
+      cell.onclick = "indexCell"
+
+        //  console.log(chessModel[2][4]);
 
       if ((r + c) % 2 == 0) {
         cell.style.backgroundColor = "black";
+        cell.style.color = "gainsboro";
+
       } if ((r + c) % 2 == 1) {
         cell.style.backgroundColor = "gainsboro";
+        cell.style.color = "black";
+
       }
       row.appendChild(cell);
-
+    //  cell.appendChild(cellText);
     }
     table.appendChild(row);
   }
 }
-
 creatTable();
 
 
+function startGame() {
+  var cells = document.getElementsByTagName("td");
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].addEventListener("click", turnClick, false);
+  }
+}
+startGame();
+const huPlayer = "O";
+function turnClick(square) {
+  turn(square.target.id, huPlayer);
+}
+function turn(squareId, player) {
+   chessBoard[squareId] = player;
+   document.getElementById(squareId).innerText = player;
+}
+/*
+
+function update () {
+  var cellElement = document.getElementById ("0_0")
+  cellElement.textContent = chessBoard[0][0];
+    if (cellElement.textContent = chessBoard) {
+      for (var i = 0; i <= chessBoard.length; i++) {
+    //  console.log( 1 + "_" + i);
+
+      }
+    } else {
+      console.log(chessBoard);
+  }
+  console.log(cellElement);
+}
+chessBoard[0][0] = "Bishop"
+update();
 
 
 
+/*
+var state = false;
+var currentPiece;
+var currentCell;
+var cells =  document.getElementsByTagName("td");
+for (var i = 0; i < cells.length; i++) {
+  cells[i].onclick = function() {
+    getCell(this);
+  };
+}
 
+function getCell(that) {
+  if (!state) {
+    state = true;
+    currentPiece = that.cells;
+    currentCell = that;
+  } else {
+    that.cells = currentPiece;
+    currentCell.cells = "";
+    state = false;
 
+  }
+}
+/*
+function allowDrop (ev) {
+  ev.preventDefault();
+}
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementsByTagName(data))
+}
+*/
+/*
+function setPieces() {
+  kingPiece = chessModel[2][1];
+  kingPiece.textContent = "King";
+  console.log(setPieces);
+}
+setPieces();
+*/
 
 
 
